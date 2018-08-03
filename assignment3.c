0ce937c826ce9160fd9e407df79b56876a1886f5
mrjeffcodes95 <mrjeffcodes95@gmail.com>
2018-08-03 00:19:56 -0500

    test the threads

--- assignment3.c
+++ assignment3.c
@@ -1,91 +1,51 @@
 #include <stdio.h>
-#include <pthread.h>
+#include <pthtread.h>
 
-#define TIMES 10
-#define CAPACITY 3
-#define ITEMSIZE 20
+float totalShirts = 1000;
+float temp;
+void *takeShirtsA();
+void *takeShirtsB();
+void *takeShirtsC();
 
-struct Buffer
+int main (int argc, char *argv[])
 {
-  int item[ITEMSIZE];
-  int size;
-};
+	pthread_t tid1, tid2, tid3;	//thread identifier
+	pthread_attr_t attr;	//thread attributes
 
-void *producer();
-void *consumer();
+	pthread_create(&tid1, NULL, (void *(*)(void*))takeShirtsA, NULL);
+	pthread_create(&tid2, NULL, (void *(*)(void*))takeShirtsB, NULL);
+	pthread_create(&tid3, NULL, (void *(*)(void*))takeShirtsC, NULL)
 
-pthread_mutex_t mutex;
-pthread_cond_t  full_cond;
-pthread_cond_t  empty_cond;
-struct Buffer buffer;
-
-int
-main()
-{ int i;
-  pthread_t tid;
-
-  for (i =0; i< ITEMSIZE; i++) buffer.item[i]=-1;
-  buffer.size = 0;
-
-  pthread_setconcurrency(2);
-
-  pthread_create(&tid, NULL, (void *(*)(void *))producer, NULL  );
-  pthread_create(&tid, NULL, (void *(*)(void *))consumer, NULL );
- 
-
- pthread_exit(0);
-
+	pthread_exit(0);
 }
 
-void *producer()
-{ int i,j;
-
- for (j = 0; j<TIMES; j++)
- {pthread_mutex_lock(&mutex);  
- if (buffer.size == CAPACITY)
-    {printf("Buffer is full. Producer is waiting...\n");
-     pthread_cond_wait(&full_cond, &mutex);
-    }
- else
-    {
-     printf("Producer adds one more item ...\n");
-     buffer.item[buffer.size++] = buffer.size;
-     for (i =0;i<buffer.size; i++)
-     printf("%d ", buffer.item[i]);
-     printf("\n");
-     pthread_cond_broadcast(&empty_cond);
-     
-    }
-  pthread_mutex_unlock(&mutex);
-}
-}
-
-void *consumer()
-{ int i,j;
-
-for (j=0; j<TIMES; j++)
-{
-  pthread_mutex_lock(&mutex);
-
-  if (buffer.size == 0)
-    {
-      printf("Buffer is empty. Consumer is waiting...\n");
-      pthread_cond_wait(&empty_cond, &mutex);
-    }
-  else
-    {
-      printf("Consumer consumes an item from buffer...\n");
-      buffer.size--;
-      for (i=0; i<buffer.size; i++)
-      printf("%d ", buffer.item[i]);
-      printf("\n");
-      pthread_cond_broadcast(&full_cond);
-    }
-
-   pthread_mutex_unlock(&mutex);
- }
- 
-}
-
-
-
+void *takeShirtsA(){
+	while (totalShirts > 0){
+		temp = 0;
+		 temp = ( totalShirts / 3.0 );
+		 if (temp != (int)temp){
+		 	totalShirts -= (totalShirts (3 - 1) / 3) + 1;
+		 }
+		 printf("A takes away %d T-shirts", temp);
+	}
+} 
+void *takeShirtsB(){
+	while (totalShirts > 0){
+		temp = 0;
+		 temp -= ( totalShirts / 4.0 );
+		 if (temp != (int)temp){
+		 	totalShirts -= (totalShirts (4 - 1) / 4);
+		 }
+		 printf("B takes away %d T-shirts", temp);
+	}
+} 
+void *takeShirtsC(){
+	while (totalShirts > 0){
+		temp = 0;
+		 temp -= ( totalShirts / 5.0 );
+		 if (temp != (int)temp){
+		 	totalShirts -= (totalShirts (4 - 1) / 4);
+		 }
+		 printf("C takes away %d T-shirts", temp);
+	}
+} 
